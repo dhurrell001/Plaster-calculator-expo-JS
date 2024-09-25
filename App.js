a93eb3b84a801f89fc70974ec36ca8f625310d9f
David Hurrell <dhurrell@hotmail.co.uk>
2024-09-25 15:59:43 +0100

    Add header image, Add textInput comp.

--- App.js
+++ App.js
@@ -1,10 +1,17 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import HeadImage from "./components/headerImage";
+import LabeledTextInput from "./components/textInput";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.title}>Plaster Calculator</Text>
+      <HeadImage />
+      <LabeledTextInput label={"Please enter Width : "} placeholder={"0"} />
+      <LabeledTextInput label={"Please enter Length : "} placeholder={"0"} />
+      <LabeledTextInput label={"Please enter thickness : "} placeholder={"0"} />
+
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +20,14 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    flexDirection: "column",
+    // backgroundColor: "blue",
+    alignItems: "center",
+    marginTop: 30,
+    // justifyContent: "space-around",
+  },
+  title: {
+    fontSize: 25,
+    marginBottom: 10,
   },
 });
