0c65366f210b8ff4fe902de86b255abe51b17b02
David Hurrell <dhurrell@hotmail.co.uk>
2024-09-27 10:43:25 +0100

    Star creating a component that displays all input fields in one view for easier management and to declutter root view App.js. Going to start new branch before I try to implement it.

---
+++ components/inputDisplayArea.jsx
@@ -0,0 +1,13 @@
+import React, { useState } from "react";
+import {
+  SafeAreaView,
+  View,
+  Text,
+  TextInput,
+  StyleSheet,
+  Keyboard,
+} from "react-native";
+
+const function InputDisplayArea({lengthInput,widthInput,setLengthInput,setWidthInput}){
+
+};
